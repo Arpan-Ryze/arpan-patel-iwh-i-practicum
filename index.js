4ac4d001643202cfadf42600a81406bf3770a6e9
Arpan Patel <arpan.patel@codersunlimited.com>
2025-04-14 12:23:14 +0530

    Form to add a new custom object record

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
 const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
 
+// ROUTE 1
 app.get('/', async (req, res) => {
     const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}?properties=name,bio,type`;
     const headers = {
@@ -30,4 +31,9 @@ app.get('/', async (req, res) => {
         console.error('Error fetching records:', error.response?.data || error.message);
         res.status(500).send('Failed to fetch records');
     }
+});
+
+// ROUTE 2:
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
