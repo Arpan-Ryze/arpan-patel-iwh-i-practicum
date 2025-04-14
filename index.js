e133df1d98de0d5b8eae6baa5152cbc2bf92b5d6
Arpan Patel <arpan.patel@codersunlimited.com>
2025-04-14 12:17:58 +0530

    authentication .env commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
+const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
